68268e559c03a645c9d56cd37d148cba8a86d34c
c.oerschkes <c.oerschkes@sgservice.de>
2022-08-29 07:53:03 +0000

    Remove the user model

--- model/model.go
+++ model/model.go
@@ -6,17 +6,9 @@ import (
 )
 
 type Note struct {
-	gorm.Model           // Adds some metadata fields to the table
-	ID         uuid.UUID `gorm:"type:uuid"` // Explicitly specify the type to be uuid
-	Title      string
-	SubTitle   string
-	Text       string
-	EditorID   uuid.UUID `gorm:"type:uuid;foreignKey:EditorID;references:ID"`
-}
-
-type User struct {
 	gorm.Model
-	ID       uuid.UUID `gorm:"type:uuid"`
-	Name     string
-	Forename string
+	ID       uuid.UUID `gorm:"primary_key;type:uuid;"`
+	Title    string
+	SubTitle string
+	Text     string
 }
--- router/router.go
+++ router/router.go
@@ -2,7 +2,6 @@ package router
 
 import (
 	noteRoutes "github.com/coerschkes/fiber-learning/internal/routes/note"
-	userRoutes "github.com/coerschkes/fiber-learning/internal/routes/user"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"gorm.io/gorm"
@@ -12,8 +11,6 @@ func SetupRoutes(app *fiber.App, database *gorm.DB) {
 	api := app.Group("/api", logger.New())
 
 	noteRouter := noteRoutes.NewNoteRouter(database)
-	userRouter := userRoutes.NewUserRouter(database)
 
 	noteRouter.SetupNoteRoutes(api)
-	userRouter.SetupNoteRoutes(api)
 }
